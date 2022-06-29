d5f375aa9343a488fc38f826e123bd5196676439
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-06-28 20:53:55 -0400

    add new type 2d icon arrays

--- ironmon_tracker/Images.lua
+++ ironmon_tracker/Images.lua
@@ -1,9 +1,12 @@
-Images = {}
+Images = {
+    typeIconImageArray = {}
+}
 
 ImageTypes = {
     GEAR = "gear",
     PHYSICAL = "physical",
-    SPECIAL = "special"
+    SPECIAL = "special",
+    STATUS = "status"
 }
 
 function Images.drawImage(imageType,x,y)
@@ -15,10 +18,12 @@ function Images.drawImage(imageType,x,y)
         imageArray = Images.getPhysicalArray()
     elseif imageType == ImageTypes.SPECIAL then
         imageArray = Images.getSpecialArray()
+    elseif imageType == ImageTypes.STATUS then
+        imageArray = Images.getStatusArray()
     end
 
     local shadow = 0x00000000
-    if imageType == ImageTypes.PHYSICAL or imageType == ImageTypes.SPECIAL then
+    if imageType == ImageTypes.PHYSICAL or imageType == ImageTypes.SPECIAL or imageType == ImageTypes.STATUS then
         shadow = Drawing.calcShadowColor(GraphicConstants.layoutColors["Bottom box background color"])
     else
         shadow = Drawing.calcShadowColor(GraphicConstants.layoutColors["Top box background color"])
@@ -29,6 +34,7 @@ function Images.drawImage(imageType,x,y)
             local offsetY = rowIndex - 1
             local color = imageArray[rowIndex][colIndex]
             if color ~= -1 then
+                color = GraphicConstants.layoutColors[imageArray[rowIndex][colIndex]]
                 if Settings.ColorSettings.Draw_shadows then gui.drawPixel(x+offsetX+1,y+offsetY+1,shadow) end
                 gui.drawPixel(x+offsetX,y+offsetY,color)
             end
@@ -36,8 +42,32 @@ function Images.drawImage(imageType,x,y)
     end
 end
 
+function Images.drawTypeIcon(moveType,x,y)
+    if moveType ~= "---" then
+        local imageArray = Images.typeIconImageArray[moveType]
+        for rowIndex = 1, #imageArray, 1 do
+            for colIndex = 1,#(imageArray[1]) do
+                local offsetX = colIndex - 1
+                local offsetY = rowIndex - 1
+                local color = imageArray[rowIndex][colIndex]
+                if color ~= -1 then
+                    color = GraphicConstants.layoutColors["Default text color"]
+                    if Settings.ColorSettings.Color_move_type_icons then 
+                        color = GraphicConstants.TYPECOLORS[moveType] 
+                    end
+                    local shadow = Drawing.calcShadowColor(GraphicConstants.layoutColors["Bottom box background color"])
+                    if Settings.ColorSettings.Draw_shadows then
+                        gui.drawPixel(x+offsetX+1,y+offsetY+1,shadow)
+                    end
+                    gui.drawPixel(x+offsetX,y+offsetY,color)
+                end
+            end
+        end
+    end
+end
+
 function Images.getPhysicalArray()
-    local c = GraphicConstants.layoutColors["Physical icon color"]
+    local c = "Physical icon color"
     local x = -1
     local physicalImageArray = {
         {c,x,x,c,x,x,c},
@@ -52,7 +82,7 @@ function Images.getPhysicalArray()
 end
 
 function Images.getSpecialArray()
-    local c = GraphicConstants.layoutColors["Special icon color"]
+    local c = "Special icon color"
     local x = -1
     local specialImageArray = {
         {x,x,c,c,c,x,x},
@@ -67,7 +97,7 @@ function Images.getSpecialArray()
 end
 
 function Images.getGearArray()
-    local c = GraphicConstants.layoutColors["Gear icon color"]
+    local c = "Gear icon color"
     local x = -1
     local gearImageArray = {
         {x,x,x,c,c,x,x,x},
@@ -80,4 +110,208 @@ function Images.getGearArray()
         {x,x,x,c,c,x,x,x}
     }
     return gearImageArray
+end
+
+
+function Images.getStatusArray()
+    local c = "Default text color"
+    local x = -1
+    local statusImageArray = {
+        {x,c,c,c,c,c,x},
+        {c,c,x,c,c,c,c},
+        {c,x,x,c,c,c,c},
+        {c,x,x,x,c,c,c},
+        {c,x,x,x,x,c,c},
+        {c,c,x,x,c,c,c},
+        {x,c,c,c,c,c,x}
+    }
+    return statusImageArray
+end
+
+--[[
+{x,c,c,c,c,c,x},
+        {c,c,x,c,c,c,c},
+        {c,x,x,c,c,c,c},
+        {c,x,x,x,c,c,c},
+        {c,x,x,x,x,c,c},
+        {c,c,x,x,c,c,c},
+        {x,c,c,c,c,c,x}
+--]]
+
+function Images.initializeTypeIconArray()
+    --Always drawn as the background shaded color, so we just need 1 and -1.
+    --c = color, x = blank
+    local c = 1
+    local x = -1
+    Images.typeIconImageArray["fire"] = {
+        {x,x,x,c,x,x,x,x},
+        {x,x,c,c,x,c,x,x},
+        {x,c,x,c,x,c,c,x},
+        {c,x,x,x,c,x,x,c},
+        {c,x,x,x,x,x,x,c},
+        {c,x,x,x,x,x,x,c},
+        {x,c,x,x,x,x,c,x},
+        {x,x,c,c,c,c,x,x},
+    }
+    Images.typeIconImageArray["water"] = {
+        {x,x,x,c,x,x,x,x,x},
+        {x,x,c,x,c,x,x,x,x},
+        {x,x,c,x,c,x,x,x,x},
+        {x,c,x,x,x,c,x,x,x},
+        {x,c,x,x,x,c,x,x,x},
+        {x,c,x,x,x,c,x,x,x},
+        {x,c,x,x,x,c,x,x,x},
+        {x,x,c,c,c,x,x,x,x},
+    }
+    Images.typeIconImageArray["grass"] = {
+        {x,x,x,c,x,x,x,x},
+        {x,x,c,x,c,x,x,x},
+        {x,c,x,x,x,c,x,x},
+        {c,x,x,x,x,x,c,x},
+        {c,x,x,x,x,x,c,x},
+        {c,x,x,x,x,x,c,x},
+        {x,c,c,x,c,c,x,x},
+        {x,x,x,c,x,x,x,x},
+    }
+    Images.typeIconImageArray["electric"] = {
+        {x,x,x,c,c,c,x,x},
+        {x,x,c,x,x,c,x,x},
+        {x,c,x,x,c,c,c,c},
+        {c,x,x,x,x,x,x,c},
+        {c,c,c,c,c,x,x,c},
+        {x,x,x,c,x,x,c,x},
+        {x,x,c,x,x,c,x,x},
+        {x,x,c,c,c,x,x,x},
+    }
+    Images.typeIconImageArray["ghost"] = {
+        {x,x,c,c,c,x,x,x},
+        {x,c,x,x,x,c,x,x},
+        {c,x,c,x,c,x,c,x},
+        {c,x,x,x,x,x,c,x},
+        {c,x,x,x,x,x,c,x},
+        {c,x,x,x,x,x,c,x},
+        {c,x,c,x,c,x,c,x},
+        {x,c,x,c,x,c,x,x},
+    }
+    Images.typeIconImageArray["bug"] = {
+        {x,x,c,x,x,c,x,x},
+        {c,x,x,c,c,x,x,c},
+        {x,c,c,x,x,c,c,x},
+        {x,c,x,x,x,x,c,x},
+        {c,c,x,x,x,x,c,c},
+        {x,c,x,x,x,x,c,x},
+        {x,x,c,x,x,c,x,x},
+        {x,c,x,c,c,x,c,x},
+    }
+    Images.typeIconImageArray["poison"] = {
+        {x,c,c,x,x,x,x,x},
+        {c,x,x,c,x,x,x,x},
+        {c,x,x,c,x,x,c,x},
+        {x,c,c,x,x,c,x,c},
+        {x,x,x,x,x,x,c,x},
+        {x,x,c,c,x,x,x,x},
+        {x,c,x,x,c,x,x,x},
+        {x,x,c,c,x,x,x,x},
+    }
+    Images.typeIconImageArray["normal"] = {
+        {x,x,c,c,c,c,x,x},
+        {x,c,x,x,x,x,c,x},
+        {c,x,x,c,c,x,x,c},
+        {c,x,c,x,x,c,x,c},
+        {c,x,c,x,x,c,x,c},
+        {c,x,x,c,c,x,x,c},
+        {x,c,x,x,x,x,c,x},
+        {x,x,c,c,c,c,x,x},
+    }
+    Images.typeIconImageArray["dark"] = {
+        {x,c,c,x,x,c,c,x},
+        {c,x,c,x,x,c,x,c},
+        {c,x,x,c,c,x,x,c},
+        {c,x,x,x,x,x,x,c},
+        {c,x,x,x,x,x,x,c},
+        {x,c,x,x,x,x,c,x},
+        {x,x,c,c,c,c,x,x},
+        {x,x,x,x,x,x,x,x},
+    }
+    Images.typeIconImageArray["ice"] = {
+        {x,x,x,c,x,x,x,x},
+        {x,c,x,c,x,c,x,x},
+        {x,x,c,x,c,x,x,x},
+        {c,c,x,c,x,c,c,x},
+        {x,x,c,x,c,x,x,x},
+        {x,c,x,c,x,c,x,x},
+        {x,x,x,c,x,x,x,x},
+        {x,x,x,x,x,x,x,x},
+    }
+    Images.typeIconImageArray["psychic"] = {
+        {x,x,c,c,c,c,x,x},
+        {x,c,x,x,x,x,c,x},
+        {c,x,x,c,c,x,x,c},
+        {c,x,c,x,c,x,x,c},
+        {c,x,c,x,x,x,c,x},
+        {c,x,x,c,c,c,x,x},
+        {x,c,x,x,x,x,x,c},
+        {x,x,c,c,c,c,c,x},
+    }
+    Images.typeIconImageArray["rock"] = {
+        {x,x,c,c,c,x,x,x},
+        {x,c,x,x,x,c,x,x},
+        {x,c,x,x,x,c,c,x},
+        {c,x,x,x,c,x,x,c},
+        {c,x,x,c,x,x,x,c},
+        {c,x,x,c,x,x,x,c},
+        {x,c,x,c,x,c,c,x},
+        {x,x,c,c,c,x,x,x},
+    }
+    Images.typeIconImageArray["ground"] = {
+        {x,x,x,x,x,x,x,x},
+        {x,x,x,c,c,x,x,x},
+        {x,x,c,x,x,c,x,x},
+        {x,x,c,x,c,c,x,x},
+        {x,c,c,c,x,x,c,x},
+        {x,c,x,x,x,x,c,x},
+        {c,x,x,x,x,x,x,c},
+        {c,c,c,c,c,c,c,c},
+    }
+    --next two are questionable
+    Images.typeIconImageArray["steel"] = {
+        {x,x,x,x,x,x,x,x},
+        {x,c,c,x,c,c,x,x},
+        {c,x,x,x,x,x,c,x},
+        {c,x,c,c,c,x,c,x},
+        {x,x,x,c,x,x,x,x},
+        {x,x,x,c,x,x,x,x},
+        {x,c,x,x,x,c,x,x},
+        {x,x,c,c,c,x,x,x},
+    }
+    Images.typeIconImageArray["fighting"] = {
+        {x,x,x,c,x,c,x,x},
+        {x,x,c,x,c,x,c,x},
+        {x,c,c,x,c,x,c,x},
+        {c,x,c,x,c,x,c,x},
+        {c,x,c,x,x,x,c,x},
+        {c,x,x,x,x,x,c,x},
+        {x,c,x,x,x,x,c,x},
+        {x,x,c,c,c,c,x,x},
+    }
+    Images.typeIconImageArray["dragon"] = {
+        {c,x,c,x,x,c,x,c},
+        {c,c,c,x,x,c,c,c},
+        {c,x,c,c,c,c,x,c},
+        {c,x,x,x,x,x,x,c},
+        {x,c,x,x,x,x,c,x},
+        {x,x,c,x,x,c,x,x},
+        {x,x,c,x,x,c,x,x},
+        {x,x,x,c,c,x,x,x},
+    }
+    Images.typeIconImageArray["flying"] = {
+        {x,x,c,c,c,c,c,x},
+        {x,c,x,x,x,x,x,c},
+        {c,x,x,x,c,c,c,c},
+        {c,x,x,c,x,x,x,c},
+        {c,x,x,x,x,c,c,x},
+        {c,x,c,c,c,x,x,x},
+        {c,c,x,x,x,x,x,x},
+        {c,x,x,x,x,x,x,x}
+    }
 end
