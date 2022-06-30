5867c4d2f66290036553ebf4652babc7b198e180
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-06-30 16:21:25 -0400

    forgot to also change white address

--- ironmon_tracker/GameSettings.lua
+++ ironmon_tracker/GameSettings.lua
@@ -77,7 +77,7 @@ function GameSettings.setAsWhite()
 	GameSettings.playerBase = 0x2349B4+0x20
 	GameSettings.playerBattleBase = 0x26A794+0x20
 	GameSettings.enemyBase = 0x26B254+0x20
-	GameSettings.playerBattleMonPID = 0x2a7e70+0x20
+	GameSettings.playerBattleMonPID = 0x2a7e14+0x20
 	GameSettings.enemyBattleMonPID = 0x2A7E14+0x20
 	GameSettings.battleStatus = 0x1D0798+0x20
 	GameSettings.itemStartNoBattle = 0x234784+0x20
