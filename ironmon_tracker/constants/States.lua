f8beaf4b2c2fe391d5fdaf4f37b8bf789b5e9c47
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-07-31 17:08:30 -0400

    add States constants

---
+++ ironmon_tracker/constants/States.lua
@@ -0,0 +1,17 @@
+States.PROGRAM =
+    MiscUtils.readOnly(
+    {
+        MAIN_SCREEN = "Main Screen",
+        MAIN_OPTIONS = "Main Options",
+        COLOR_EDITING = "Color Editing",
+        COLOR_PICKER = "Color Picker"
+    }
+)
+
+States.INPUT =
+    MiscUtils.readOnly(
+    {
+        READ_INPUT = "Read Input",
+        IGNORE_INPUT = "Ignore Input"
+    }
+)
