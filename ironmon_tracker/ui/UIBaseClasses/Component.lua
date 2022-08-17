30f601ef839cb647b9fdfd0e8a098b0229fea4d5
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-08-17 00:45:36 -0400

    create Component UI class

---
+++ ironmon_tracker/ui/UIBaseClasses/Component.lua
@@ -0,0 +1,38 @@
+local function Component(initialFrame, initialBox)
+    local self = {}
+
+    local frame = initialFrame
+    local box = initialBox
+
+    function self.calculateActualPosition(position)
+        box.calculateActualPosition(position)
+    end
+
+    function self.show()
+        box.show()
+    end
+
+    function self.getSize()
+        return box.getSize()
+    end
+
+    function self.getFrame()
+        return frame
+    end
+
+    function self.getPosition()
+        return box.getPosition()
+    end
+
+    function self.move(newPosition)
+        box.move(newPosition)
+    end
+
+    function self.shift(xAmount, yAmount)
+        box.shift(xAmount, yAmount)
+    end
+
+    return self
+end
+
+return Component
