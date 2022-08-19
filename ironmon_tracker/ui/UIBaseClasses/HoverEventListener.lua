152a069e08d19354236e6de27200b9b2b1a41c83
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-08-19 17:49:55 -0400

    add HoverEventListener class

---
+++ ironmon_tracker/ui/UIBaseClasses/HoverEventListener.lua
@@ -0,0 +1,42 @@
+local function HoverEventListener(control, onHoverFunction, initialOnHoverParams, onHoverEndFunction, initialOnHoverEndParams)
+    local self = {}
+    local onHover = onHoverFunction
+    local onHoverParams = initialOnHoverParams
+    local onHoverEnd = onHoverEndFunction
+    local onHoverEndParams = initialOnHoverEndParams
+    local framesWaited = 0
+    local hoverActive = false
+    function self.setOnHoverParams(newOnHoverParams)
+        onHoverParams = newOnHoverParams
+    end
+    function self.setOnHoverEndParams(newOnHoverEndParams)
+        onHoverEndParams = newOnHoverEndParams
+    end
+    function self.listen()
+        local position = control.getPosition()
+        local size = control.getSize()
+        local mouse = input.getmouse()
+        local mouseX = mouse["X"]
+        local mouseY = (mouse["Y"] + 384) / 2
+        local inRange = MiscUtils.mouseInRange(mouseX, mouseY, position.x, position.y, size.width, size.height)
+        if not hoverActive then
+            if inRange then
+                framesWaited = framesWaited + 1
+                if framesWaited == 30 then
+                    onHover(onHoverParams)
+                    hoverActive = true
+                end
+            else
+                framesWaited = 0
+            end
+        else
+            if not inRange then
+                onHoverEnd(onHoverEndParams)
+                hoverActive = false
+            end
+        end
+    end
+    return self
+end
+
+return HoverEventListener
