9ac4091d8ba2423ee0cb25c820ef7a10bc165b1c
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-08-20 03:55:12 -0400

    take certain constants out of GameSettings and move them to a separate file in the constants folder

---
+++ ironmon_tracker/constants/GameInfo.lua
@@ -0,0 +1,58 @@
+GameInfo = {}
+
+GameInfo.GAME_INFO = {
+	[GameInfo.VERSION_NUMBER.DIAMOND] = {
+		GEN = 4,
+		NAME = "Pokemon Diamond",
+		BADGE_PREFIX = "DPPT",
+		VERSION_GROUP = 8
+	},
+	[GameInfo.VERSION_NUMBER.PEARL] = {
+		GEN = 4,
+		NAME = "Pokemon Pearl",
+		BADGE_PREFIX = "DPPT",
+		VERSION_GROUP = 8
+	},
+	[GameInfo.VERSION_NUMBER.PLATINUM] = {
+		GEN = 4,
+		NAME = "Pokemon Platinum",
+		BADGE_PREFIX = "DPPT",
+		VERSION_GROUP = 9
+	},
+	[GameInfo.VERSION_NUMBER.HEART_GOLD] = {
+		GEN = 4,
+		NAME = "Pokemon HeartGold",
+		BADGE_PREFIX = "HGSS",
+		VERSION_GROUP = 10
+	},
+	[GameInfo.VERSION_NUMBER.SOUL_SILVER = {
+		GEN = 4,
+		NAME = "Pokemon SoulSilver",
+		BADGE_PREFIX = "HGSS",
+		VERSION_GROUP = 10
+	},
+	[GameInfo.VERSION_NUMBER.BLACK] = {
+		GEN = 5,
+		NAME = "Pokemon Black",
+		BADGE_PREFIX = "BW",
+		VERSION_GROUP = 11
+	},
+	[GameInfo.VERSION_NUMBER.WHITE] = {
+		GEN = 5,
+		NAME = "Pokemon White",
+		BADGE_PREFIX = "BW",
+		VERSION_GROUP = 11
+	},
+	[GameInfo.VERSION_NUMBER.BLACK2] = {
+		GEN = 5,
+		NAME = "Pokemon Black 2",
+		BADGE_PREFIX = "BW2",
+		VERSION_GROUP = 12
+	},
+	[GameInfo.VERSION_NUMBER.WHITE2 = {
+		GEN = 5,
+		NAME = "Pokemon White 2",
+		BADGE_PREFIX = "BW2",
+		VERSION_GROUP = 12
+	}
+}
