d12d141cb4d0b9fcb9b39066124cd2fcce53a4ac
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-08-21 03:56:21 -0400

    add GameInfo constant to retrieve various constant data about games

--- ironmon_tracker/constants/GameInfo.lua
+++ ironmon_tracker/constants/GameInfo.lua
@@ -17,54 +17,63 @@ GameInfo.GAME_INFO = {
         GEN = 4,
         NAME = "Pokemon Diamond",
         BADGE_PREFIX = "DPPT",
-        VERSION_GROUP = 8
+        VERSION_GROUP = 8,
+        ENCRYPTED_POKEMON_SIZE = 236
     },
     [GameInfo.VERSION_NUMBER.PEARL] = {
         GEN = 4,
         NAME = "Pokemon Pearl",
         BADGE_PREFIX = "DPPT",
-        VERSION_GROUP = 8
+        VERSION_GROUP = 8,
+        ENCRYPTED_POKEMON_SIZE = 236
     },
     [GameInfo.VERSION_NUMBER.PLATINUM] = {
         GEN = 4,
         NAME = "Pokemon Platinum",
         BADGE_PREFIX = "DPPT",
-        VERSION_GROUP = 9
+        VERSION_GROUP = 9,
+        ENCRYPTED_POKEMON_SIZE = 236
     },
     [GameInfo.VERSION_NUMBER.HEART_GOLD] = {
         GEN = 4,
         NAME = "Pokemon HeartGold",
         BADGE_PREFIX = "HGSS",
-        VERSION_GROUP = 10
+        VERSION_GROUP = 10,
+        ENCRYPTED_POKEMON_SIZE = 236
     },
     [GameInfo.VERSION_NUMBER.SOUL_SILVER] = {
         GEN = 4,
         NAME = "Pokemon SoulSilver",
         BADGE_PREFIX = "HGSS",
-        VERSION_GROUP = 10
+        VERSION_GROUP = 10,
+        ENCRYPTED_POKEMON_SIZE = 236
     },
     [GameInfo.VERSION_NUMBER.BLACK] = {
         GEN = 5,
         NAME = "Pokemon Black",
         BADGE_PREFIX = "BW",
-        VERSION_GROUP = 11
+        VERSION_GROUP = 11,
+        ENCRYPTED_POKEMON_SIZE = 220
     },
     [GameInfo.VERSION_NUMBER.WHITE] = {
         GEN = 5,
         NAME = "Pokemon White",
         BADGE_PREFIX = "BW",
-        VERSION_GROUP = 11
+        VERSION_GROUP = 11,
+        ENCRYPTED_POKEMON_SIZE = 220
     },
     [GameInfo.VERSION_NUMBER.BLACK2] = {
         GEN = 5,
         NAME = "Pokemon Black 2",
         BADGE_PREFIX = "BW2",
-        VERSION_GROUP = 12
+        VERSION_GROUP = 12,
+        ENCRYPTED_POKEMON_SIZE = 220
     },
     [GameInfo.VERSION_NUMBER.WHITE2] = {
         GEN = 5,
         NAME = "Pokemon White 2",
         BADGE_PREFIX = "BW2",
-        VERSION_GROUP = 12
+        VERSION_GROUP = 12,
+        ENCRYPTED_POKEMON_SIZE = 220
     }
 }
