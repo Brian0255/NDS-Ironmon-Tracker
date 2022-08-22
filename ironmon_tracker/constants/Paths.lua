134db07b72364eade43e9208b1a97e37e66f2563
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-08-22 04:11:54 -0400

    add type images path

--- ironmon_tracker/constants/Paths.lua
+++ ironmon_tracker/constants/Paths.lua
@@ -5,5 +5,6 @@ Paths.FOLDERS = {
     UI_FOLDER = "ironmon_tracker/ui",
     UI_BASE_CLASSES = "ironmon_tracker/ui/UIBaseClasses",
     UTILS_FOLDER = "ironmon_tracker/utils",
-    CONSTANTS_FOLDER = "ironmon_tracker/constants"
+    CONSTANTS_FOLDER = "ironmon_tracker/constants",
+    TYPE_IMAGES_FOLDER = "ironmon_tracker/images/types",
 }
