501c5a810c6d83f11a88606c679dde68765b59a0
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-08-23 05:10:38 -0400

    create battle options UI

---
+++ ironmon_tracker/ui/BattleOptionsScreen.lua
@@ -0,0 +1,181 @@
+local function BattleOptionsScreen(initialSettings, initialTracker, initialProgram)
+    local Frame = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/Frame.lua")
+    local Box = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/Box.lua")
+    local Component = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/cOMPONENT.lua")
+    local TextLabel = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/TextLabel.lua")
+    local ImageLabel = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/ImageLabel.lua")
+    local ImageField = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/ImageField.lua")
+    local TextField = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/TextField.lua")
+    local TextStyle = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/TextStyle.lua")
+    local Layout = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/Layout.lua")
+    local Icon = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/Icon.lua")
+    local HoverEventListener = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/HoverEventListener.lua")
+    local MouseClickEventListener = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/MouseClickEventListener.lua")
+    local SettingToggleButton = dofile(Paths.FOLDERS.UI_BASE_CLASSES .. "/SettingToggleButton.lua")
+    local settings = initialSettings
+    local tracker = initialTracker
+    local program = initialProgram
+    local constants = {
+        BATTLE_OPTIONS_HEIGHT = 110,
+        TOGGLE_FRAME_WIDTH = 200,
+        TOGGLE_FRAME_HEIGHT = 12,
+        BUTTON_SIZE = 10
+    }
+    local ui = {}
+    local eventListeners = {}
+    local self = {}
+    local function onGoBackClick()
+        program.setCurrentScreens({program.UI_SCREENS.MAIN_OPTIONS_SCREEN})
+        program.drawCurrentScreens()
+    end
+    local function initEventListeners()
+        --eventListeners.goBackClickListener = MouseClickEventListener(ui.controls.goBackButton, onGoBackClick)
+    end
+    local function onToggleClick(button)
+        button.onClick()
+        program.drawCurrentScreens()
+    end
+    local function initBattleToggleButtons()
+        local orderedKeys = {
+            "SHOW_MOVE_EFFECTIVENESS",
+            "CALCULATE_VARIABLE_DAMAGE",
+            "SHOW_ACTUAL_ENEMY_PP",
+            "SHOW_1ST_FIGHT_STATS_PLATINUM"
+        }
+        for _, key in pairs(orderedKeys) do
+            local frame =
+                Frame(
+                Box(
+                    {x = 0, y = 0},
+                    {width = constants.TOGGLE_FRAME_WIDTH, height = constants.TOGGLE_FRAME_HEIGHT},
+                    nil,
+                    nil
+                ),
+                Layout(Graphics.ALIGNMENT_TYPE.HORIZONTAL, 2),
+                ui.frames.mainInnerFrame
+            )
+            local toggle =
+                SettingToggleButton(
+                Component(
+                    frame,
+                    Box(
+                        {x = 0, y = 0},
+                        {width = constants.BUTTON_SIZE, height = constants.BUTTON_SIZE},
+                        "Top box background color",
+                        "Top box border color",
+                        true,
+                        "Top box background color"
+                    )
+                ),
+                settings.battle,
+                key,
+                nil,
+                false
+            )
+            local labelName
+            if key == "SHOW_1ST_FIGHT_STATS_PLATINUM" then
+                labelName = "Platinum: Show 1st fight stats"
+            else
+                labelName = key:gsub("_", " "):lower()
+                labelName = labelName:sub(1, 1):upper() .. labelName:sub(2)
+            end
+            TextLabel(
+                Component(frame, Box({x = 0, y = 0}, {width = 0, height = 0}, nil, nil, false)),
+                TextField(
+                    labelName,
+                    {x = 0, y = 0},
+                    TextStyle(
+                        Graphics.FONT.DEFAULT_FONT_SIZE,
+                        Graphics.FONT.DEFAULT_FONT_FAMILY,
+                        "Default text color",
+                        "Top box background color"
+                    )
+                )
+            )
+            table.insert(eventListeners, MouseClickEventListener(toggle, onToggleClick, toggle))
+        end
+    end
+    local function initUI()
+        ui.controls = {}
+        ui.frames = {}
+        ui.frames.mainFrame =
+            Frame(
+            Box(
+                {x = Graphics.SIZES.SCREEN_WIDTH, y = 0},
+                {width = Graphics.SIZES.MAIN_SCREEN_WIDTH, height = constants.BATTLE_OPTIONS_HEIGHT},
+                "Main background color",
+                nil
+            ),
+            nil,
+            nil
+        )
+        ui.frames.mainInnerFrame =
+            Frame(
+            Box(
+                {x = Graphics.SIZES.BORDER_MARGIN, y = Graphics.SIZES.BORDER_MARGIN},
+                {
+                    width = Graphics.SIZES.MAIN_SCREEN_WIDTH - 2 * Graphics.SIZES.BORDER_MARGIN,
+                    height = constants.BATTLE_OPTIONS_HEIGHT - 2 * Graphics.SIZES.BORDER_MARGIN
+                },
+                "Top box background color",
+                "Top box border color"
+            ),
+            Layout(Graphics.ALIGNMENT_TYPE.VERTICAL, 2, {x = 4, y = 22}),
+            ui.frames.mainFrame
+        )
+        ui.controls.topHeading =
+            TextLabel(
+            Component(
+                ui.frames.mainFrame,
+                Box(
+                    {x = 5, y = 5},
+                    {width = Graphics.SIZES.MAIN_SCREEN_WIDTH - 2 * Graphics.SIZES.BORDER_MARGIN, height = 18},
+                    "Top box background color",
+                    "Top box border color",
+                    false
+                )
+            ),
+            TextField(
+                "Battle Options",
+                {x = 30, y = 1},
+                TextStyle(13, Graphics.FONT.DEFAULT_FONT_FAMILY, "Default text color", "Top box background color")
+            )
+        )
+        ui.controls.goBackButton =
+            TextLabel(
+            Component(
+                ui.frames.mainFrame,
+                Box(
+                    {x = Graphics.SIZES.MAIN_SCREEN_WIDTH - 49, y = constants.BATTLE_OPTIONS_HEIGHT - 23},
+                    {width = 40, height = 14},
+                    "Top box background color",
+                    "Top box border color", true, "Top box background color"
+                )
+            ),
+            TextField(
+                "Go back",
+                {x = 3, y = 1},
+                TextStyle(
+                    Graphics.FONT.DEFAULT_FONT_SIZE,
+                    Graphics.FONT.DEFAULT_FONT_FAMILY,
+                    "Default text color",
+                    "Top box background color"
+                )
+            )
+        )
+        table.insert(eventListeners,MouseClickEventListener(ui.controls.goBackButton,onGoBackClick))
+        initBattleToggleButtons()
+    end
+    function self.runEventListeners()
+        for _, eventListener in pairs(eventListeners) do
+            eventListener.listen()
+        end
+    end
+    function self.show()
+        ui.frames.mainFrame.show()
+    end
+    initUI()
+    return self
+end
+
+return BattleOptionsScreen
