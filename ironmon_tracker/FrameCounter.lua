65805306d357a20a64657d4cb9a008d79b5518c0
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-09-05 02:21:46 -0400

    add option to attempt to sync with client framerate

--- ironmon_tracker/FrameCounter.lua
+++ ironmon_tracker/FrameCounter.lua
@@ -1,11 +1,21 @@
-local function FrameCounter(frameAmount, onZeroEvent, onZeroParams)
+local function FrameCounter(frameAmount, onZeroEvent, onZeroParams, syncWithClient)
     local self = {}
     local totalFrames = frameAmount
     local currentFrames = totalFrames
+    if syncWithClient == nil then
+        syncWithClient = false
+    end
     function self.decrement()
         currentFrames = currentFrames - 1
-        if currentFrames == 0 then
+        if currentFrames <= 0 then
             currentFrames = totalFrames
+            if syncWithClient then
+                --throttle if it's > 60 to cut down on lag
+                local clientFrameRate = client.get_approx_framerate()
+                if clientFrameRate > 60 then
+                    currentFrames = totalFrames * (clientFrameRate / 60)
+                end
+            end
             onZeroEvent(onZeroParams)
         end
     end
