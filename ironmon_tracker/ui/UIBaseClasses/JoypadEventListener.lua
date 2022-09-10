2592fcb5875a017509d24156f865c3120ba1fcdf
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-09-10 07:20:06 -0400

    add a way to listen for any button

--- ironmon_tracker/ui/UIBaseClasses/JoypadEventListener.lua
+++ ironmon_tracker/ui/UIBaseClasses/JoypadEventListener.lua
@@ -1,14 +1,34 @@
-local function JoypadEventListener(controllerSettings, buttonKey, initialOnButtonPress, initialOnButtonPressParams)
+local function JoypadEventListener(
+    controllerSettings,
+    initialButtonKey,
+    initialOnButtonPress,
+    initialOnButtonPressParams)
     local self = {}
     local lastInput = nil
     local onButtonPress = initialOnButtonPress
     local onButtonPressParams = initialOnButtonPressParams
+    local buttonKey = initialButtonKey
     function self.listen()
         local joypadButtons = joypad.get()
-        local button = controllerSettings[buttonKey]
-        if lastInput ~= nil then
-            if lastInput[button] and joypadButtons[button] ~= lastInput[button] then
-                onButtonPress(onButtonPressParams)
+        if buttonKey == "ANY" then
+            if lastInput ~= nil then
+                for button, _ in pairs(joypadButtons) do
+                    if button ~= "Touch" and lastInput[button] and joypadButtons[button] ~= lastInput[button] then
+                        if onButtonPressParams ~= nil then
+                            onButtonPressParams.button = button
+                        else
+                            onButtonPressParams = button
+                        end
+                        onButtonPress(onButtonPressParams)
+                    end
+                end
+            end
+        else
+            local button = controllerSettings[buttonKey]
+            if lastInput ~= nil then
+                if lastInput[button] and joypadButtons[button] ~= lastInput[button] then
+                    onButtonPress(onButtonPressParams)
+                end
             end
         end
         lastInput = joypadButtons
