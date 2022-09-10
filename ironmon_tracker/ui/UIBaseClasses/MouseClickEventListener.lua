7efc294f033bb1247346b446a38307ac6b1f381f
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-09-10 11:47:01 -0400

    add ability to use it to detect a mouse click anywhere

--- ironmon_tracker/ui/UIBaseClasses/MouseClickEventListener.lua
+++ ironmon_tracker/ui/UIBaseClasses/MouseClickEventListener.lua
@@ -1,7 +1,8 @@
-local function MouseClickEventListener(control, onClickFunction, initialOnClickParams)
+local function MouseClickEventListener(initialControl, onClickFunction, initialOnClickParams)
     local self = {}
     local onClick = onClickFunction
     local onClickParams = initialOnClickParams
+    local control = initialControl
     local previouslyPressed = nil
     function self.getOnClickParams()
         return onClickParams
@@ -10,18 +11,28 @@ local function MouseClickEventListener(control, onClickFunction, initialOnClickP
         onClickParams = newParams
     end
     function self.listen()
-        local position = control.getPosition()
-        local size = control.getSize()
-        local mousePosition = Input.getMousePosition()
+        local inRange = true
+        if control ~= nil then
+            local mousePosition = Input.getMousePosition()
+            local position = control.getPosition()
+            local size = control.getSize()
+            inRange =
+                MiscUtils.mouseInRange(
+                mousePosition.x,
+                mousePosition.y,
+                position.x,
+                position.y,
+                size.width,
+                size.height
+            )
+        end
         local leftPress = input.getmouse()["Left"]
-        local inRange =
-            MiscUtils.mouseInRange(mousePosition.x, mousePosition.y, position.x, position.y, size.width, size.height)
-            if previouslyPressed ~= nil then
-                if previouslyPressed and not leftPress and inRange then
-                    onClick(onClickParams)
-                end
+        if previouslyPressed ~= nil then
+            if previouslyPressed and not leftPress and inRange then
+                onClick(onClickParams)
             end
-            previouslyPressed = leftPress
+        end
+        previouslyPressed = leftPress
     end
     return self
 end
