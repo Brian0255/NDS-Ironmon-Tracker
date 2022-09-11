27b5548ac1db8e2e8f6df84679bd5eddc86e538e
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-09-11 01:31:05 -0400

    sync hover frame to client framerate correctly

--- ironmon_tracker/ui/UIBaseClasses/HoverEventListener.lua
+++ ironmon_tracker/ui/UIBaseClasses/HoverEventListener.lua
@@ -12,10 +12,7 @@ local function HoverEventListener(
     local framesWaited = 0
     local hoverActive = false
     local baseWaitAmount = 30
-    local clientFrameRate = client.get_approx_framerate()
-    if clientFrameRate > 60 then
-        --baseWaitAmount = math.max(200,baseWaitAmount * (clientFrameRate / 60))
-    end
+    local currentWaitAmount = baseWaitAmount
     function self.reset()
         hoverActive = false
         framesWaited = 0
@@ -32,6 +29,17 @@ local function HoverEventListener(
     function self.getOnHoverEndParams()
         return onHoverEndParams
     end
+    local function resetCurrentAmount()
+        local clientFrameRate = client.get_approx_framerate()
+        if clientFrameRate ~= nil then
+            if clientFrameRate > 60 then
+                currentWaitAmount = math.floor(baseWaitAmount * (clientFrameRate / 60))
+            end
+        else
+            currentWaitAmount = baseWaitAmount
+        end
+    end
+    resetCurrentAmount()
     function self.listen()
         local position = control.getPosition()
         local size = control.getSize()
@@ -41,9 +49,10 @@ local function HoverEventListener(
         if not hoverActive then
             if inRange then
                 framesWaited = framesWaited + 1
-                if framesWaited == baseWaitAmount then
+                if framesWaited == currentWaitAmount then
                     onHover(onHoverParams)
                     hoverActive = true
+                    resetCurrentAmount()
                 end
             else
                 framesWaited = 0
