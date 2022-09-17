5a60017d9bd75f11b2a41768f5bc767c5d81e88f
ninjafriend <jtigues@gmail.com>
2022-09-16 23:50:45 -0500

    Testing index

--- ironmon_tracker/ui/MainScreen.lua
+++ ironmon_tracker/ui/MainScreen.lua
@@ -1478,22 +1478,42 @@ local function MainScreen(initialSettings, initialTracker, initialProgram)
 
     local function getImageForStatus(status)
         local imgName = ""
-        if status == 0 then --None
-            return imgName
-        elseif status < 8 then -- Sleep
-            imgName = "SLP"
-        elseif status == 8 then -- Poison
-            imgName = "PSN"
-        elseif status == 16 then -- Burn
-            imgName = "BRN"
-        elseif status == 32 then -- Freeze
-            imgName = "FRZ"
-        elseif status == 64 then -- Paralyze
-            imgName = "PAR"
-        elseif status == 128 then -- Toxic Poison
-            imgName = "PSN"
-        else
-            return imgName
+        if program.getGameInfo().GEN == 4 then
+            if status == 0 then --None
+                return imgName
+            elseif status < 8 then -- Sleep
+                imgName = "SLP"
+            elseif status == 8 then -- Poison
+                imgName = "PSN"
+            elseif status == 16 then -- Burn
+                imgName = "BRN"
+            elseif status == 32 then -- Freeze
+                imgName = "FRZ"
+            elseif status == 64 then -- Paralyze
+                imgName = "PAR"
+            elseif status == 128 then -- Toxic Poison
+                imgName = "PSN"
+            else
+                return imgName
+            end
+        elseif program.getGameInfo().GEN == 5 then
+            if status == 0 then --None
+                return imgName
+            elseif status == 1 then -- Paralyze
+                imgName = "PAR"
+            elseif status == 2 then -- Sleep
+                imgName = "SLP"
+            elseif status == 3 then -- Freeze
+                imgName = "FRZ"
+            elseif status == 4 then -- Burn
+                imgName = "BRN"
+            elseif status == 5 then -- Poison
+                imgName = "PSN"
+            elseif status == 6 then -- Toxic Poison
+                imgName = "PSN"
+            else
+                return imgName
+            end
         end
         statusPath = "ironmon_tracker/images/status/" .. imgName .. ".png"
         return statusPath
