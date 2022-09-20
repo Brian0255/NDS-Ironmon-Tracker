4cf2ceac09b7c0e46771e8502a61d526e434d107
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-09-20 13:21:52 -0400

    update version number and small fix

--- ironmon_tracker/constants/MiscConstants.lua
+++ ironmon_tracker/constants/MiscConstants.lua
@@ -1,3 +1,68 @@
 MiscConstants = {}
 
-MiscConstants.TRACKER_VERSION = "0.4.1f"
+MiscConstants.TRACKER_VERSION = "0.4.2"
+
+MiscConstants.DEFAULT_SETTINGS = {
+    appearance = {
+        RIGHT_JUSTIFIED_NUMBERS = true,
+        SHOW_POKECENTER_HEALS = false,
+        SHOW_ACCURACY_AND_EVASION = true,
+        CLICKABLE_INFO_HOVERING = false,
+        DISPLAY_HOVERS_TO_THE_RIGHT = false,
+        ICON_SET_INDEX=1
+    },
+    controls = {
+        CHANGE_VIEW = "Start",
+        ENABLE_ENEMY_LOCKING  = false,
+        LOAD_NEXT_SEED = "A B Start Select",
+        CYCLE_STAT = "L",
+        CYCLE_PREDICTION = "R"
+    },
+    battle = {
+        CALCULATE_VARIABLE_DAMAGE = true,
+        SHOW_MOVE_EFFECTIVENESS = true,
+        SHOW_ACTUAL_ENEMY_PP = true,
+        SHOW_1ST_FIGHT_STATS_PLATINUM = true,
+        ENABLE_ENEMY_LOCKING = false,
+    },
+    colorScheme = {
+        ["Main background color"] = 4278190080,
+        ["Top box text color"] = 4294967295,
+        ["Bottom box text color"] = 4294967295,
+        ["Positive text color"] = 4278255360,
+        ["Negative text color"] = 4294901760,
+        ["Intermediate text color"]=4294967040,
+        ["Move header text color"] = 4294967295,
+        ["Top box border color"] = 4289374890,
+        ["Bottom box border color"] = 4289374890,
+        ["Top box background color"] = 4280427042,
+        ["Bottom box background color"] = 4280427042,
+        ["Physical icon color"] = 4294952497,
+        ["Special icon color"] = 4286428927,
+        ["Gear icon color"] = 4292598747,
+        ["Black"] = 4278190080
+    },
+    colorSettings = {
+        ["Color move names by type"] = true,
+        ["Draw shadows"] = true,
+        [" Draw move type icons"] = false,
+        [" Color move type icons"] = false,
+        [" Transparent backgrounds"] = false,
+        [" Show phys/spec move icons"] = true
+    },
+    quickLoad = {
+        LOAD_TYPE = "GENERATE_ROMS",
+        ROMS_FOLDER_PATH = "",
+        ROM_PATH = "",
+        JAR_PATH = "",
+        SETTINGS_PATH = ""
+    },
+    badgesAppearance = {
+        SINGLE_BADGE_ALIGNMENT = "RIGHT",
+        DOUBLE_BADGE_ALIGNMENT = "LEFT_AND_RIGHT",
+        SHOW_BOTH_BADGES = true,
+        SPACER = true,
+        PRIMARY_DOUBLE_BADGE_SET = "JOHTO",
+        PRIMARY_BADGE_SET = "JOHTO"
+    }
+}
