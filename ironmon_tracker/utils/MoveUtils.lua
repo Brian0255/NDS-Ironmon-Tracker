8cab296d8ee4f5c0efd45e77eff1bfa3013f9fcd
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-09-21 10:28:42 -0400

    fix gen 5 doom desire/future sight bug

--- ironmon_tracker/utils/MoveUtils.lua
+++ ironmon_tracker/utils/MoveUtils.lua
@@ -20,11 +20,6 @@ function MoveUtils.netEffectiveness(move, pkmnData, isEnemy, hiddenPowerType)
         end
         return 1.0
     end
-
-    if move["name"] == "Future Sight" or move["name"] == "Doom Desire" then
-        return 1.0
-    end
-
     local effectiveness = 1.0
     for _, type in ipairs(pkmnData["type"]) do
         local moveType = move.type
