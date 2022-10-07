e96f8915df6bd84ebb794d268fde5d2eccc0b0e3
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-10-07 11:00:15 -0400

    add support for 2 line author for explorers set

--- ironmon_tracker/ui/PokemonIconsScreen.lua
+++ ironmon_tracker/ui/PokemonIconsScreen.lua
@@ -20,7 +20,7 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
         POKEMON_ICON_WIDTH = 31,
         POKEMON_ICON_HEIGHT = 28,
         TEXT_HEADER_HEIGHT = 18,
-        ICON_FRAME_HEIGHT = 82,
+        ICON_FRAME_HEIGHT = 90,
         FORWARD_BACK_BUTTON_WIDTH = 12,
         BOTTOM_FRAME_HEIGHT = 24,
         NAV_BUTTON_WIDTH = 12,
@@ -35,36 +35,46 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
     local function changeIndexToRandomID()
         currentPokemonIndex = math.random(1, #PokemonData.POKEMON - 1)
     end
-    
+
     local function getCenterStart(totalWidth, labelName)
-        return (totalWidth - DrawingUtils.calculateWordPixelLength(labelName))/2
+        return (totalWidth - DrawingUtils.calculateWordPixelLength(labelName)) / 2
     end
 
     local function setAuthorLabel()
-        local author = currentIconSet.AUTHOR
-        local labelName = "Credit: "..author
-        ui.controls.authorLabel.setText(labelName)
-        local offset = {
-            x = getCenterStart(ui.controls.authorLabel.getSize().width-2, labelName),
-            y = 1
-        }
-        ui.controls.authorLabel.setTextOffset(offset)
+        local authorLines = currentIconSet.AUTHOR
+        for i, label in pairs(ui.controls.authorLabels) do
+            if authorLines[i] then
+                label.setText(authorLines[i])
+            else
+                label.setText("")
+            end
+            local offset = {
+                x = getCenterStart(label.getSize().width - 2, label.getText()),
+                y = 1
+            }
+            label.setTextOffset(offset)
+        end
     end
 
     local function randomizeIcon()
         changeIndexToRandomID()
-        DrawingUtils.readPokemonIDIntoImageLabel(currentIconSet, currentPokemonIndex, ui.controls.pokemonImageLabel, currentIconSet.CHOOSE_IMAGE_OFFSET)
+        DrawingUtils.readPokemonIDIntoImageLabel(
+            currentIconSet,
+            currentPokemonIndex,
+            ui.controls.pokemonImageLabel,
+            currentIconSet.CHOOSE_IMAGE_OFFSET
+        )
+        program.drawCurrentScreens()
+    end
+
+    local function setUpIconSet()
+        currentIconSet = IconSets.SETS[settings.appearance.ICON_SET_INDEX]
         local totalWidth = constants.NAV_LABEL_WIDTH
         local name = currentIconSet.NAME
         local xOffset = (totalWidth - DrawingUtils.calculateWordPixelLength(name)) / 2
         ui.controls.nameLabel.setText(currentIconSet.NAME)
-        ui.controls.nameLabel.setTextOffset({x=xOffset - 1,y=1})
+        ui.controls.nameLabel.setTextOffset({x = xOffset - 1, y = 1})
         setAuthorLabel()
-        program.drawCurrentScreens()
-    end
-
-    local function changeIconSet()
-        currentIconSet = IconSets.SETS[settings.appearance.ICON_SET_INDEX]
         frameCounters["changeIndex"].reset()
         randomizeIcon()
         program.drawCurrentScreens()
@@ -73,7 +83,7 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
     local function onForwardClick()
         local currentIndex = settings.appearance.ICON_SET_INDEX
         settings.appearance.ICON_SET_INDEX = (currentIndex % #IconSets.SETS) + 1
-        changeIconSet()
+        setUpIconSet()
     end
 
     local function onBackwardClick()
@@ -81,7 +91,7 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
         local totalSets = #IconSets.SETS
         local currentIndex = settings.appearance.ICON_SET_INDEX
         settings.appearance.ICON_SET_INDEX = ((currentIndex + totalSets - 2) % totalSets) + 1
-        changeIconSet()
+        setUpIconSet()
     end
 
     local function onGoBackClick()
@@ -161,8 +171,8 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
                 TextStyle(10, Graphics.FONT.DEFAULT_FONT_FAMILY, "Top box text color", "Top box background color")
             )
         )
-        table.insert(eventListeners,MouseClickEventListener(ui.controls.goForwardButton, onForwardClick))
-        table.insert(eventListeners,MouseClickEventListener(ui.controls.goBackwardButton, onBackwardClick))
+        table.insert(eventListeners, MouseClickEventListener(ui.controls.goForwardButton, onForwardClick))
+        table.insert(eventListeners, MouseClickEventListener(ui.controls.goBackwardButton, onBackwardClick))
     end
 
     local function setUpPokemonIcon()
@@ -188,11 +198,45 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
                     {x = 0, y = 0},
                     {width = constants.POKEMON_ICON_WIDTH, height = constants.POKEMON_ICON_HEIGHT},
                     "Top box background color",
-                    "Top box border color" , true,"Top box background color"
+                    "Top box border color",
+                    true,
+                    "Top box background color"
                 )
             ),
             ImageField("", {x = 0, y = -5}, nil)
         )
+    end
+
+    local function setUpAuthorLabels()
+        ui.controls.authorLabels = {}
+        for i = 1, 2, 1 do
+            ui.controls.authorLabels[i] =
+                TextLabel(
+                Component(
+                    ui.frames.iconFrame,
+                    Box(
+                        {x = 0, y = 0},
+                        {
+                            width = Graphics.SIZES.MAIN_SCREEN_WIDTH - 2 * Graphics.SIZES.BORDER_MARGIN,
+                            height = 13
+                        },
+                        nil,
+                        nil,
+                        false
+                    )
+                ),
+                TextField(
+                    "",
+                    {x = 0, y = 0},
+                    TextStyle(
+                        Graphics.FONT.DEFAULT_FONT_SIZE,
+                        Graphics.FONT.DEFAULT_FONT_FAMILY,
+                        "Top box text color",
+                        "Top box background color"
+                    )
+                )
+            )
+        end
     end
 
     local function initUI()
@@ -264,32 +308,7 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
         )
         setupNavFrameButtons()
         setUpPokemonIcon()
-        ui.controls.authorLabel = 
-        TextLabel(
-        Component(
-            ui.frames.iconFrame,
-            Box(
-                {x = 0, y = 0},
-                {
-                    width = Graphics.SIZES.MAIN_SCREEN_WIDTH-2*Graphics.SIZES.BORDER_MARGIN,
-                    height = 0
-                },
-                nil,
-                nil,
-                false
-            )
-        ),
-        TextField(
-            "",
-            {x = 0, y = 0},
-            TextStyle(
-                Graphics.FONT.DEFAULT_FONT_SIZE,
-                Graphics.FONT.DEFAULT_FONT_FAMILY,
-                "Top box text color",
-                "Top box background color"
-            )
-        )
-    )
+        setUpAuthorLabels()
         ui.frames.goBackFrame =
             Frame(
             Box(
@@ -346,6 +365,7 @@ local function PokemonIconsScreen(initialSettings, initialTracker, initialProgra
 
     function self.initialize()
         randomizeIcon()
+        setUpIconSet()
     end
 
     function self.show()
