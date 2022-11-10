d6e75814e2c59f2b1b03c01a0295a20113ef31b5
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-11-10 17:11:35 -0500

    minor

--- Ironmon-Tracker.lua
+++ Ironmon-Tracker.lua
@@ -1,6 +1,5 @@
 -- NDS Ironnon Tracker
 -- Created by OnlySpaghettiCode, largely based on the Ironmon Tracker by besteon and other contributors
-
 IronmonTracker = {}
 local Main = dofile("ironmon_tracker/Main.lua")
 function IronmonTracker.startTracker()
