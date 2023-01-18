d1e42132e5770d5cd00b87e1f7f882a807e938ce
OnlySpaghettiCode <brian.neveu2@gmail.com>
2023-01-18 04:30:54 -0500

    update game info with new references

--- ironmon_tracker/constants/GameInfo.lua
+++ ironmon_tracker/constants/GameInfo.lua
@@ -12,396 +12,6 @@ GameInfo.VERSION_NUMBER = {
     WHITE2 = 0x4F445249
 }
 
-local LocationData = {
-    [GameInfo.VERSION_NUMBER.BLACK] = {
-        [0] = {
-            name = "Black City",
-            encounterSlots = nil
-        },
-        [6] = {
-            name = "Striaton City",
-            encounterSlots = nil
-        },
-        [7] = {
-            name = "the first gym",
-            encountersSlots = nil
-        },
-        [16] = {
-            name = "Nacrene City",
-            encounterSlots = nil
-        },
-        [18] = {
-            name = "the second gym",
-            encounterSlots = nil
-        },
-        [19] = {
-            name = "the second gym",
-            encounterSlots = nil
-        },
-        [28] = {
-            name = "Castelia City",
-            encounterSlots = nil
-        },
-        [62] = {
-            name = "Nimbasa City",
-            encounterSlots = nil
-        },
-        [96] = {
-            name = "Driftveil City",
-            encounterSlots = nil
-        },
-        [107] = {
-            name = "Mistralton City",
-            encounterSlots = nil
-        },
-        [113] = {
-            name = "Icirrus City",
-            encounterSlots = nil
-        },
-        [120] = {
-            name = "Opelucid City",
-            encounterSlots = nil
-        },
-        [136] = {
-            name = "Pok\233mon League",
-            encounterSlots = nil
-        },
-        [147] = {
-            name = "Unity Tower",
-            encounterSlots = nil
-        },
-        [152] = {
-            name = "Dreamyard",
-            encounterSlots = nil
-        },
-        [154] = {
-            name = "Pinwheel Forest Exterior",
-            encounterSlots = nil
-        },
-        [155] = {
-            name = "Pinwheel Forest Interior",
-            encounterSlots = nil
-        },
-        [157] = {
-            name = "Desert Resort",
-            encounterSlots = nil
-        },
-        [160] = {
-            name = "Relic Castle",
-            encounterSlots = nil
-        },
-        [191] = {
-            name = "Cold Storage",
-            encounterSlots = nil
-        },
-        [194] = {
-            name = "Chargestone Cave",
-            encounterSlots = nil
-        },
-        [198] = {
-            name = "Twist Mountain",
-            encounterSlots = nil
-        },
-        [205] = {
-            name = "Dragonspiral Tower",
-            encounterSlots = nil
-        },
-        [214] = {
-            name = "Victory Road",
-            encounterSlots = nil
-        },
-        [230] = {
-            name = "Giant Chasm",
-            encounterSlots = nil
-        },
-        [235] = {
-            name = "Liberty Garden",
-            encounterSlots = nil
-        },
-        [238] = {
-            name = "P2 Laboratory",
-            encounterSlots = nil
-        },
-        [240] = {
-            name = "Undella Bay",
-            encounterSlots = nil
-        },
-        [241] = {
-            name = "Abyssal Ruins",
-            encounterSlots = nil
-        },
-        [249] = {
-            name = "Skyarrow Bridge",
-            encounterSlots = nil
-        },
-        [253] = {
-            name = "Driftveil Drawbridge",
-            encounterSlots = nil
-        },
-        [254] = {
-            name = "Tubeline Bridge",
-            encounterSlots = nil
-        },
-        [255] = {
-            name = "Village Bridge",
-            encounterSlots = nil
-        },
-        [263] = {
-            name = "Marvelous Bridge",
-            encounterSlots = nil
-        },
-        [264] = {
-            name = "N's Castle",
-            encounterSlots = nil
-        },
-        [279] = {
-            name = "Entree Forest",
-            encounterSlots = nil
-        },
-        [289] = {
-            name = "Nimbasa City",
-            encounterSlots = nil
-        },
-        [290] = {
-            name = "Driftveil City",
-            encounterSlots = nil
-        },
-        [291] = {
-            name = "Mistralton City",
-            encounterSlots = nil
-        },
-        [292] = {
-            name = "Icirrus City",
-            encounterSlots = nil
-        },
-        [293] = {
-            name = "Opelucid City",
-            encounterSlots = nil
-        },
-        [294] = {
-            name = "Black City",
-            encounterSlots = nil
-        },
-        [295] = {
-            name = "White Forest",
-            encounterSlots = nil
-        },
-        [296] = {
-            name = "Cold Storage",
-            encounterSlots = nil
-        },
-        [297] = {
-            name = "Chargestone Cave",
-            encounterSlots = nil
-        },
-        [298] = {
-            name = "Twist Mountain",
-            encounterSlots = nil
-        },
-        [299] = {
-            name = "Dragonspiral Tower",
-            encounterSlots = nil
-        },
-        [300] = {
-            name = "Giant Chasm",
-            encounterSlots = nil
-        },
-        [301] = {
-            name = "Driftveil Drawbridge",
-            encounterSlots = nil
-        },
-        [302] = {
-            name = "Tubeline Bridge",
-            encounterSlots = nil
-        },
-        [303] = {
-            name = "Marvelous Bridge",
-            encounterSlots = nil
-        },
-        [304] = {
-            name = "Route 5",
-            encounterSlots = nil
-        },
-        [305] = {
-            name = "Route 6",
-            encounterSlots = nil
-        },
-        [306] = {
-            name = "Route 7",
-            encounterSlots = nil
-        },
-        [307] = {
-            name = "Route 8",
-            encounterSlots = nil
-        },
-        [308] = {
-            name = "Route 9",
-            encounterSlots = nil
-        },
-        [309] = {
-            name = "Route 11",
-            encounterSlots = nil
-        },
-        [310] = {
-            name = "Route 12",
-            encounterSlots = nil
-        },
-        [311] = {
-            name = "Route 13",
-            encounterSlots = nil
-        },
-        [312] = {
-            name = "Route 14",
-            encounterSlots = nil
-        },
-        [313] = {
-            name = "Route 15",
-            encounterSlots = nil
-        },
-        [314] = {
-            name = "Route 16",
-            encounterSlots = nil
-        },
-        [315] = {
-            name = "Lacunosa Town",
-            encounterSlots = nil
-        },
-        [316] = {
-            name = "Undella Town",
-            encounterSlots = nil
-        },
-        [317] = {
-            name = "Route 1",
-            encounterSlots = nil
-        },
-        [319] = {
-            name = "Route 2",
-            encounterSlots = nil
-        },
-        [321] = {
-            name = "Route 3",
-            encounterSlots = nil
-        },
-        [324] = {
-            name = "Wellspring Cave",
-            encounterSlots = nil
-        },
-        [326] = {
-            name = "Route 4",
-            encounterSlots = nil
-        },
-        [329] = {
-            name = "Route 5",
-            encounterSlots = nil
-        },
-        [331] = {
-            name = "Route 6",
-            encounterSlots = nil
-        },
-        [333] = {
-            name = "Mistralton Cave",
-            encounterSlots = nil
-        },
-        [337] = {
-            name = "Route 7",
-            encounterSlots = nil
-        },
-        [338] = {
-            name = "Celestial Tower",
-            encounterSlots = nil
-        },
-        [345] = {
-            name = "Route 8",
-            encounterSlots = nil
-        },
-        [346] = {
-            name = "Moor of Icirrus",
-            encounterSlots = nil
-        },
-        [348] = {
-            name = "Route 9",
-            encounterSlots = nil
-        },
-        [352] = {
-            name = "Challenger's Cave",
-            encounterSlots = nil
-        },
-        [355] = {
-            name = "Route 10",
-            encounterSlots = nil
-        },
-        [365] = {
-            name = "Route 11",
-            encounterSlots = nil
-        },
-        [368] = {
-            name = "Route 12",
-            encounterSlots = nil
-        },
-        [370] = {
-            name = "Route 13",
-            encounterSlots = nil
-        },
-        [374] = {
-            name = "Route 14",
-            encounterSlots = nil
-        },
-        [376] = {
-            name = "Abundant Shrine",
-            encounterSlots = nil
-        },
-        [378] = {
-            name = "Route 15",
-            encounterSlots = nil
-        },
-        [381] = {
-            name = "Pok\233 Transfer Lab",
-            encounterSlots = nil
-        },
-        [383] = {
-            name = "Route 16",
-            encounterSlots = nil
-        },
-        [385] = {
-            name = "Lostlorn Forest",
-            encounterSlots = nil
-        },
-        [387] = {
-            name = "Route 18",
-            encounterSlots = nil
-        },
-        [389] = {
-            name = "Nuvema Town",
-            encounterSlots = nil
-        },
-        [397] = {
-            name = "Accumula Town",
-            encounterSlots = nil
-        },
-        [406] = {
-            name = "Lacunosa Town",
-            encounterSlots = nil
-        },
-        [412] = {
-            name = "Undella Town",
-            encounterSlots = nil
-        },
-        [418] = {
-            name = "Anville Town",
-            encounterSlots = nil
-        },
-        [423] = {
-            name = "Route 17",
-            encounterSlots = nil
-        },
-        [424] = {
-            name = "White Forest",
-            encounterSlots = nil
-        },
-    }
-}
-
-
 GameInfo.GAME_INFO = {
     [GameInfo.VERSION_NUMBER.DIAMOND] = {
         GEN = 4,
@@ -409,8 +19,10 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "DPPT",
         VERSION_GROUP = 1,
         ENEMY_PARTY_OFFSET = 0xB60,
-        ACTIVE_PID_DIFFERENCE=0x180,
-        ENCRYPTED_POKEMON_SIZE = 236
+        ACTIVE_PID_DIFFERENCE = 0x180,
+        ENCRYPTED_POKEMON_SIZE = 236,
+        GYM_TMS = {76, 86, 60, 55, 65, 91, 72, 57},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.DIAMOND]
     },
     [GameInfo.VERSION_NUMBER.PEARL] = {
         GEN = 4,
@@ -418,8 +30,10 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "DPPT",
         VERSION_GROUP = 1,
         ENEMY_PARTY_OFFSET = 0xB60,
-        ACTIVE_PID_DIFFERENCE=0x180,
-        ENCRYPTED_POKEMON_SIZE = 236
+        ACTIVE_PID_DIFFERENCE = 0x180,
+        ENCRYPTED_POKEMON_SIZE = 236,
+        GYM_TMS = {76, 86, 60, 55, 65, 91, 72, 57},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.DIAMOND]
     },
     [GameInfo.VERSION_NUMBER.PLATINUM] = {
         GEN = 4,
@@ -427,8 +41,10 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "DPPT",
         VERSION_GROUP = 2,
         ENEMY_PARTY_OFFSET = 0xB60,
-        ACTIVE_PID_DIFFERENCE=0x180,
-        ENCRYPTED_POKEMON_SIZE = 236
+        ACTIVE_PID_DIFFERENCE = 0x180,
+        ENCRYPTED_POKEMON_SIZE = 236,
+        GYM_TMS = {76, 86, 60, 55, 65, 91, 72, 57},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.DIAMOND]
     },
     [GameInfo.VERSION_NUMBER.HEART_GOLD] = {
         GEN = 4,
@@ -436,8 +52,11 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "HGSS",
         VERSION_GROUP = 3,
         ENEMY_PARTY_OFFSET = 0xBA0,
-        ACTIVE_PID_DIFFERENCE=0x180,
-        ENCRYPTED_POKEMON_SIZE = 236
+        ACTIVE_PID_DIFFERENCE = 0x180,
+        ENCRYPTED_POKEMON_SIZE = 236,
+        GYM_TMS = {51, 89, 45, 30, 01, 23, 07, 59, -1, 34, 48, 19, 03, 84, 80, 50, 92},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.HEART_GOLD],
+        LOCATION_DATA = LocationData.LOCATION_DATA[GameInfo.VERSION_NUMBER.HEART_GOLD]
     },
     [GameInfo.VERSION_NUMBER.SOUL_SILVER] = {
         GEN = 4,
@@ -445,8 +64,10 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "HGSS",
         VERSION_GROUP = 3,
         ENEMY_PARTY_OFFSET = 0xBA0,
-        ACTIVE_PID_DIFFERENCE=0x180,
-        ENCRYPTED_POKEMON_SIZE = 236
+        ACTIVE_PID_DIFFERENCE = 0x180,
+        ENCRYPTED_POKEMON_SIZE = 236,
+        GYM_TMS = {51, 89, 45, 30, 01, 23, 07, 59, -1, 34, 48, 19, 03, 84, 80, 50, 92},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.HEART_GOLD]
     },
     [GameInfo.VERSION_NUMBER.BLACK] = {
         GEN = 5,
@@ -454,9 +75,11 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "BW",
         VERSION_GROUP = 4,
         ENEMY_PARTY_OFFSET = 0xAC0,
-        ACTIVE_PID_DIFFERENCE=0x5C,
+        ACTIVE_PID_DIFFERENCE = 0x5C,
         ENCRYPTED_POKEMON_SIZE = 220,
-        LOCATION_DATA = LocationData[GameInfo.VERSION_NUMBER.BLACK]
+        LOCATION_DATA = LocationData.LOCATION_DATA[GameInfo.VERSION_NUMBER.BLACK],
+        GYM_TMS = {83, 67, 76, 72, 78, 62, 79, 82},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.BLACK]
     },
     [GameInfo.VERSION_NUMBER.WHITE] = {
         GEN = 5,
@@ -464,8 +87,10 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "BW",
         VERSION_GROUP = 4,
         ENEMY_PARTY_OFFSET = 0xAC0,
-        ACTIVE_PID_DIFFERENCE=0x5C,
-        ENCRYPTED_POKEMON_SIZE = 220
+        ACTIVE_PID_DIFFERENCE = 0x5C,
+        ENCRYPTED_POKEMON_SIZE = 220,
+        GYM_TMS = {83, 67, 76, 72, 78, 62, 79, 82},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.WHITE]
     },
     [GameInfo.VERSION_NUMBER.BLACK2] = {
         GEN = 5,
@@ -473,8 +98,10 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "BW2",
         VERSION_GROUP = 5,
         ENEMY_PARTY_OFFSET = 0xAC0,
-        ACTIVE_PID_DIFFERENCE=0x5C,
-        ENCRYPTED_POKEMON_SIZE = 220
+        ACTIVE_PID_DIFFERENCE = 0x5C,
+        ENCRYPTED_POKEMON_SIZE = 220,
+        GYM_TMS = {83, 09, 76, 72, 78, 62, 82, 55},
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.BLACK2]
     },
     [GameInfo.VERSION_NUMBER.WHITE2] = {
         GEN = 5,
@@ -482,7 +109,10 @@ GameInfo.GAME_INFO = {
         BADGE_PREFIX = "BW2",
         VERSION_GROUP = 5,
         ENEMY_PARTY_OFFSET = 0xAC0,
-        ACTIVE_PID_DIFFERENCE=0x5C,
-        ENCRYPTED_POKEMON_SIZE = 220
+        ACTIVE_PID_DIFFERENCE = 0x5C,
+        ENCRYPTED_POKEMON_SIZE = 220,
+        GYM_TMS = {83, 09, 76, 72, 78, 62, 82, 55},
+        LOCATION_DATA = LocationData.LOCATION_DATA[GameInfo.VERSION_NUMBER.BLACK],
+        TRAINERS = TrainerData.TRAINERS[GameInfo.VERSION_NUMBER.BLACK2]
     }
 }
