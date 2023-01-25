63d5dcce6127ba466a540ea23bc248055f8ddbf5
OnlySpaghettiCode <brian.neveu2@gmail.com>
2023-01-25 03:33:00 -0500

    attempted fix at download jank

--- Ironmon-Tracker.lua
+++ Ironmon-Tracker.lua
@@ -1,10 +1,10 @@
 -- NDS Ironnon Tracker
 -- Created by OnlySpaghettiCode, largely based on the Ironmon Tracker by besteon and other contributors
+local Main = dofile("ironmon_tracker/Main.lua")
 IronmonTracker = {}
 function IronmonTracker.startTracker()
 	gui.clearImageCache()
 	collectgarbage()
-	local Main = dofile("ironmon_tracker/Main.lua")
 	local main = Main()
 	main.run()
 end
