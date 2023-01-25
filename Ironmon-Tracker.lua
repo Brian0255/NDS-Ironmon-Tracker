c3488f911f493ba28ff6d5256367a6d73a71b3b0
OnlySpaghettiCode <brian.neveu2@gmail.com>
2023-01-25 01:16:17 -0500

    change time of garbage collection

--- Ironmon-Tracker.lua
+++ Ironmon-Tracker.lua
@@ -2,9 +2,9 @@
 -- Created by OnlySpaghettiCode, largely based on the Ironmon Tracker by besteon and other contributors
 IronmonTracker = {}
 function IronmonTracker.startTracker()
-	local Main = dofile("ironmon_tracker/Main.lua")
 	gui.clearImageCache()
-	collectgarbage("collect")
+	collectgarbage()
+	local Main = dofile("ironmon_tracker/Main.lua")
 	local main = Main()
 	main.run()
 end
