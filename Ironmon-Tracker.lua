34d9c0433b142a4be3ac4d8981827b5fd9cae715
OnlySpaghettiCode <brian.neveu2@gmail.com>
2023-01-25 01:08:02 -0500

    another auto update fix attempt

--- Ironmon-Tracker.lua
+++ Ironmon-Tracker.lua
@@ -1,8 +1,8 @@
 -- NDS Ironnon Tracker
 -- Created by OnlySpaghettiCode, largely based on the Ironmon Tracker by besteon and other contributors
 IronmonTracker = {}
-local Main = dofile("ironmon_tracker/Main.lua")
 function IronmonTracker.startTracker()
+	local Main = dofile("ironmon_tracker/Main.lua")
 	gui.clearImageCache()
 	collectgarbage("collect")
 	local main = Main()
