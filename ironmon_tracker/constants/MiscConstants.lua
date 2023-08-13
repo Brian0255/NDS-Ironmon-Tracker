eb46f876ccfa2fe75bfdd6e892354aa436d9beb1
Brian0255 <brian.neveu2@gmail.com>
2023-08-13 15:30:59 -0400

    notes

--- ironmon_tracker/constants/MiscConstants.lua
+++ ironmon_tracker/constants/MiscConstants.lua
@@ -1,11 +1,12 @@
 MiscConstants = {}
 
-MiscConstants.TRACKER_VERSION = "5.6.0"
+MiscConstants.TRACKER_VERSION = "5.6.1"
 
 MiscConstants.BIZHAWK_VERSION = client.getversion()
 
 MiscConstants.UPDATE_NOTES = {
-    "-- Illusion spoiling has been fixed for the majority of cases in Gen 5."
+    "-- Fixed a critical error where the tracker could get stuck trying to load for Black/White 2 (thanks UTDZac).",
+    "-- Fixed a bug where the experience bar could overlap with the FRIEND meter."
 }
 
 MiscConstants.DEFAULT_SETTINGS = {
