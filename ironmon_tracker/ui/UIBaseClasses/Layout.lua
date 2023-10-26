92729ac95d4e82d5487c52781c719025b3000ca0
Brian0255 <brian.neveu2@gmail.com>
2023-10-26 04:39:59 -0400

    add support for grid type

--- ironmon_tracker/ui/UIBaseClasses/Layout.lua
+++ ironmon_tracker/ui/UIBaseClasses/Layout.lua
@@ -1,9 +1,10 @@
-local function Layout(initialAlignment, initialSpacing, initialPadding)
+local function Layout(initialAlignment, initialSpacing, initialPadding, initialMaxItemsPerRow)
     local self = {}
     local frame = nil
     local alignment = initialAlignment
     local spacing = initialSpacing
     local padding = initialPadding
+    local maxItemsPerRow = initialMaxItemsPerRow or 0
     if padding == nil then
         padding = {
             x = 0,
@@ -33,17 +34,27 @@ local function Layout(initialAlignment, initialSpacing, initialPadding)
         }
     end
     function self.changeItemPositions()
+        local currentRowItems = 0
         local startPosition = frame.getPosition()
         startPosition.x = startPosition.x + padding.x
         startPosition.y = startPosition.y + padding.y
-        local currentPosition = startPosition
+        local currentPosition = {x = startPosition.x, y = startPosition.y}
         for _, item in pairs(items) do
             if item.isVisible() then
+                currentRowItems = currentRowItems + 1
                 item.move(currentPosition)
                 if alignment == Graphics.ALIGNMENT_TYPE.HORIZONTAL then
                     currentPosition.x = currentPosition.x + item.getSize().width + spacing
                 elseif alignment == Graphics.ALIGNMENT_TYPE.VERTICAL then
                     currentPosition.y = currentPosition.y + item.getSize().height + spacing
+                elseif alignment == Graphics.ALIGNMENT_TYPE.GRID then
+                    if currentRowItems == maxItemsPerRow then
+                        currentRowItems = 0
+                        currentPosition.y = currentPosition.y + item.getSize().height + spacing
+                        currentPosition.x = startPosition.x
+                    else
+                        currentPosition.x = currentPosition.x + item.getSize().width + spacing
+                    end
                 end
             end
         end
