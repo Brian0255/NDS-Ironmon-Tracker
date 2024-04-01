48ef4e90fd204dc8f5d5c77c4df0634acb30252a
Brian0255 <brian.neveu2@gmail.com>
2024-04-01 13:26:51 -0400

    bulk of BW2 work

---
+++ ironmon_tracker/BattleHandlerGen5.lua
@@ -0,0 +1,157 @@
+BattleHandlerGen5 = BattleHandlerBase:new()
+local FrameCounter = dofile(Paths.FOLDERS.DATA_FOLDER .. "/FrameCounter.lua")
+
+function BattleHandlerGen5:_setUpBattleVariables()
+    self:_baseSetUpBattleVariables()
+    local battleData = self:getBattleData()
+    self._battlerAmount = 0
+end
+
+function BattleHandlerGen5:new(o, gameInfo, memoryAddresses, pokemonDataReader, tracker, program, settings)
+    self = BattleHandlerBase.new(self, o, gameInfo, memoryAddresses, pokemonDataReader, tracker, program, settings)
+    self._activeSlotsInBattle = 0
+    self.DOUBLE_TRIPLE_FLAG_TO_BATTLER_AMOUNT = {
+        [0] = 1,
+        [1] = 2,
+        [2] = 3,
+        [3] = 3
+    }
+    self.BATTLE_STAT_OFFSETS = {
+        ["curHP"] = 0x10,
+        ["totalHP"] = 0x0E,
+        ["level"] = 0x18,
+        ["statStages"] = 0xFC,
+        ["stats"] = 0xEE,
+        ["status"] = 0x20,
+        ["moves"] = 0x104
+    }
+    self._battlerAmount = 0
+    return self
+end
+
+function BattleHandlerGen5:_readAmountOfBattlers()
+    --more than 2 mean multi-player double/triple
+    local currentPointer = self.memoryAddresses.mainBattleDataPtr
+    local total = 0
+    while (Memory.read_u32_le(currentPointer)) ~= 0 do
+        currentPointer = currentPointer + 0x1C
+        total = total + 1
+    end
+    self._battlerAmount = total
+end
+
+function BattleHandlerGen5:_addBattlerSlot(battlerSlots, slot, battleDataPtr)
+    if slot == nil then
+        slot = #battlerSlots + 1
+    end
+    battlerSlots[slot] = {
+        ["battleDataPtr"] = battleDataPtr,
+        lastPointerValue = nil,
+        lastValidPokemon = nil,
+        currentPokemon = nil
+    }
+end
+
+function BattleHandlerGen5:_readAndAdvanceBattleDataPtr(currentBattleDataPtr, amount, isEnemy, advanceAmount)
+    local battleData = self:_getCorrectBattleData(isEnemy)
+    local endPoint = currentBattleDataPtr + ((amount - 1) * advanceAmount)
+    for addr = currentBattleDataPtr, endPoint, advanceAmount do
+        self:_addBattlerSlot(battleData.slots, nil, addr)
+    end
+    return endPoint
+end
+
+function BattleHandlerGen5:_tryToFetchBattleData()
+    local firstPlayerPartyPID = Memory.read_u32_le(self.memoryAddresses.playerBase)
+    local firstPlayerPID = Memory.read_u32_le(self.memoryAddresses.playerBattleBase)
+    local firstEnemyPID = Memory.read_u32_le(self.memoryAddresses.enemyBase)
+    if firstPlayerPID == 0 or firstEnemyPID == 0 or firstPlayerPID ~= firstPlayerPartyPID then
+        return false
+    end
+    self:_readAmountOfBattlers()
+    local currentBattleDataPtr = self.memoryAddresses.mainBattleDataPtr
+    if self._battlerAmount == 2 then
+        local doubleTriple = Memory.read_u8(self.memoryAddresses.doubleTripleFlag)
+        local activeSlots = self.DOUBLE_TRIPLE_FLAG_TO_BATTLER_AMOUNT[doubleTriple] or 1
+        self:_readAndAdvanceBattleDataPtr(currentBattleDataPtr, activeSlots, false, 0x04)
+        currentBattleDataPtr = self.memoryAddresses.mainBattleDataPtr + 0x1C
+        self:_readAndAdvanceBattleDataPtr(currentBattleDataPtr, activeSlots, true, 0x04)
+    elseif self._battlerAmount > 2 and self._battlerAmount <= 6 then
+        --multi-player double, read only the first block as player and the rest as enemies
+        --this way, if you have an ally you don't get revealed info about them
+        currentBattleDataPtr = self:_readAndAdvanceBattleDataPtr(currentBattleDataPtr, 1, false, 0x1C)
+        self:_readAndAdvanceBattleDataPtr(currentBattleDataPtr, self._battlerAmount - 1, true, 0x1C)
+    else
+        return false
+    end
+    return true
+end
+
+function BattleHandlerGen5:_readBattleStats(decryptedData, battleDataBase, isEnemy)
+    decryptedData.curHP = Memory.read_u16_le(battleDataBase + self.BATTLE_STAT_OFFSETS["curHP"])
+    decryptedData.HP = Memory.read_u16_le(battleDataBase + self.BATTLE_STAT_OFFSETS["totalHP"])
+    if not isEnemy then
+        decryptedData.level = Memory.read_u16_le(battleDataBase + self.BATTLE_STAT_OFFSETS["level"]) % 256
+        local statStart = battleDataBase + self.BATTLE_STAT_OFFSETS["stats"]
+        local stats = {"ATK", "DEF", "SPA", "SPD", "SPE"}
+        for i = 0, 4, 1 do
+            local stat = stats[i + 1]
+            decryptedData[stat] = Memory.read_u16_le(statStart + i * 2)
+        end
+    end
+    local statusStart = battleDataBase + self.BATTLE_STAT_OFFSETS["status"]
+    for i = 1, #MiscData.STATUS_TO_IMG_NAME + 1, 1 do
+        local status = Memory.read_u32_le(statusStart)
+        if status ~= 0 then
+            decryptedData.status = i
+        end
+    end
+    local movesStart = battleDataBase + self.BATTLE_STAT_OFFSETS["moves"]
+    local moveIDs = {"move1", "move2", "move3", "move4"}
+    local movePPs = {"move1PP", "move2PP", "move3PP", "move4PP"}
+    for i = 0, 3, 1 do
+        local currentMove = movesStart + i * 14
+        local moveID = Memory.read_u16_le(currentMove)
+        local movePP = Memory.read_u8(currentMove + 2)
+        decryptedData[moveIDs[i + 1]] = moveID
+        decryptedData[movePPs[i + 1]] = movePP
+    end
+    self.pokemonDataReader.setCurrentBase(battleDataBase + self.BATTLE_STAT_OFFSETS["statStages"])
+    decryptedData.statStages = self.pokemonDataReader.readBattleStatStages()
+end
+
+function BattleHandlerGen5:_getPokemonData(battleData, slotIndex, isEnemy)
+    if not self:inBattleAndFetched() then
+        return
+    end
+    local battlers = battleData.slots
+    local battler = battlers[slotIndex]
+    local battleDataBase = Memory.read_pointer(battler.battleDataPtr)
+    local pokemonDataBase = Memory.read_pointer(battleDataBase)
+    local illusionedMonPointer = battleDataBase + 0x04
+    if Memory.read_u32_le(illusionedMonPointer) ~= 0 and isEnemy then
+        pokemonDataBase = Memory.read_pointer(illusionedMonPointer)
+    end
+    self.pokemonDataReader.setCurrentBase(pokemonDataBase)
+    local data = self.pokemonDataReader.decryptPokemonInfo(false, 1, isEnemy)
+    if data == nil or next(data) == nil then
+        return
+    end
+    self:_readBattleStats(data, battleDataBase, battlers.isEnemy)
+    self._program.checkForAlternateForm(data)
+    local different = (battler.lastPointerValue == nil) or (battleDataBase ~= battler.lastPointerValue)
+    if different and isEnemy then
+        self:_logNewEnemy(data)
+        if battler.lastValidPokemon ~= nil then
+            self._tracker.updateLastLevelSeen(battler.lastValidPokemon.pokemonID, battler.lastValidPokemon.level)
+        end
+        battler.lastValidPokemon = data
+    end
+    battler.lastPointerValue = battleDataBase
+    return data
+end
+
+function BattleHandlerGen5:_isTransformed()
+    --TODO: not needed seemingly?
+    return false
+end
