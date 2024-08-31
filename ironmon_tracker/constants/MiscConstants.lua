5ffdad9944313ddc0ede876f304e02747e7ae256
Brian0255 <brian.neveu2@gmail.com>
2024-08-30 21:19:32 -0400

    version

--- ironmon_tracker/constants/MiscConstants.lua
+++ ironmon_tracker/constants/MiscConstants.lua
@@ -1,11 +1,13 @@
 MiscConstants = {}
 
-MiscConstants.TRACKER_VERSION = "6.2.6"
+MiscConstants.TRACKER_VERSION = "6.2.7"
 
 MiscConstants.BIZHAWK_VERSION = client.getversion()
-
+--7:Limber, 12:Oblivious, 15:Insomnia, 17:Immunity, 20:Own Tempo, 40:Magma Armor, 41:Water Veil, 72:Vital Spirit
 MiscConstants.UPDATE_NOTES = {
-    "-- Minor bug fixes."
+    "-- Fixed incorrect Route 204 Old Rod data in the Log Viewer.",
+    "-- Since the following abilities in Gen 4 were often revealing when they shouldn't be, they will not autotrack for the time being: Limber, Oblivious, Insomnia, Own Tempo, Magma Armor, Water Veil, and Vital Spirit.",
+    "-- If you see this happen for any other abilities not listed above, please let me know!"
 }
 
 MiscConstants.DEFAULT_SETTINGS = {
